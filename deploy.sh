301bfe68f3090ca96dec2dc2702dbee282138ea9
Fabio C. Barrioneuvo da Luz <bnafta@gmail.com>
2017-02-23 15:22:01 -0300

    outra tentativa

--- deploy.sh
+++ deploy.sh
@@ -9,15 +9,16 @@ echo -e "$VARNAME"
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     echo -e "Starting to deploy to Github Pages\n"
     chmod 600 publish-key
+    mv publish-key ~/.ssh/publish-key
     eval `ssh-agent -s`
-    ssh-add publish-key
+    ssh-add ~/.ssh/publish-key
     if [ "$TRAVIS" == "true" ]; then
 #        git config --global user.email "bnafta@gmail.com"
         git config --global user.email "bot@dojoto.xyz"
         git config --global user.name "DojoTO-Bot"
     fi
     # using token clone gh-pages branch
-    git clone --quiet --branch=$BRANCH https://${GH_TOKEN}@github.com/$TARGET_REPO built_website > /dev/null
+    git clone --quiet --branch=$BRANCH git@github.com/$TARGET_REPO built_website #> /dev/null
     # go into directory and copy data we're interested in to that directory
     cd built_website
     # limpa branch master para nao ficar arquivos lixo, como arquivos excluidos e/ou renomeados
